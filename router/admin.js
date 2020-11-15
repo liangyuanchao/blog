6971dd8f7be935355a963ce7b1fae0809c968c59
梁远朝 <1661277636@qq.com>
2020-11-16 00:00:35 +0800

    登录查询数据库用户信息

--- router/admin.js
+++ router/admin.js
@@ -1,4 +1,5 @@
 const Express = require('express');
+const {User} = require('../model/user')
 
 const admin = new Express.Router();
 
@@ -8,11 +9,23 @@ admin.get('/login', (req, res)=>{
 })
 
 // 登录路由验证
-admin.post('/login', (req,res)=>{
+admin.post('/login',async (req,res)=>{
     // 接收请求参数
     const {email, password} = req.body;
     if(email.trim().length == 0 || password.trim().length == 0) return res.status(400).render('admin/error',{msg: '邮件地址或密码错误'});
-    
+    // 根据邮箱地址查询用户地址
+    let user = await User.findOne({email});
+    if(user){
+        // 将客户端传递过来的密码或用户进行查询
+        if(password == user.password){
+            // 登陆成功
+            res.send('ok')
+        }else{
+            res.status(400).render('admin/error',{msg: '邮箱地址或密码错误'})
+        }
+    }else{
+        res.status(400).render('admin/error',{msg: '邮箱地址或密码错误'})
+    }
 })
 admin.get('/user', (req, res)=>{
     res.render('admin/user')
